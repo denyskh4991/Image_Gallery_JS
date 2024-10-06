04b5d0bec3dcee7ee02090b9fde58a65cdf632bb
denyskh4991 <denyskh4991@gmail.com>
2024-10-06 21:30:51 +0200

    Adding scrolling by mouse wheel

---
+++ script.js
@@ -0,0 +1,8 @@
+let scrollContainer = document.querySelector(".gallery");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+});
